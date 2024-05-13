94b605d74156b712170faf65487fe243d12b29a3
ProgrammerJayT <theanthem8@gmail.com>
2024-05-14 01:38:02 +0200

    Create home screen

---
+++ src/screens/home/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.root}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
