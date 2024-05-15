7f35a98d3450018508e8900400201c4af42cd847
ProgrammerJayT <theanthem8@gmail.com>
2024-05-15 12:39:58 +0200

    Add RootSiblingParent

--- App.js
+++ App.js
@@ -4,16 +4,20 @@ import RouteStack from "./src/router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
 import { axiosHeaders } from "./src/services/config/axios";
+import { RootSiblingParent } from "react-native-root-siblings";
 
 const App = () => {
   useEffect(() => {
     axiosHeaders();
+    console.log("Entry file", process.env.API_URL);
   }, []);
 
   return (
     <SafeAreaView style={styles.root}>
       <NavigationContainer>
-        <RouteStack />
+        <RootSiblingParent>
+          <RouteStack />
+        </RootSiblingParent>
       </NavigationContainer>
     </SafeAreaView>
   );
