7e283c9dedfa8c185c0410a9258a96c006fca3d6
ProgrammerJayT <theanthem8@gmail.com>
2024-05-17 04:06:05 +0200

    Update router configuration for Users and Interests Places screens

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/home";
 import WelcomeScreen from "../screens/welcome";
 import RegisterScreen from "../screens/auth/register";
-import InterestsPlacesScreen from "../screens/interests-places";
 import VisitsScreen from "../screens/visits";
-import UserLoginsScreen from "../screens/user-logins";
-import UsersScreen from "../screens/users";
-import UserScreen from "../screens/user";
+import UsersScreen from "../screens/users/all";
+import UserScreen from "../screens/users/one";
 import LoginScreen from "../screens/auth/login";
+import CreateInterestsPlace from "../screens/interests-places/create";
+import UsersLoginsScreen from "../screens/users-logins";
+import InterestsPlacesScreen from "../screens/interests-places/all";
+import InterestsPlaceScreen from "../screens/interests-places/one";
 
 const Stack = createNativeStackNavigator();
 
@@ -21,21 +23,35 @@ const RouteStack = () => {
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ gestureEnabled: false }}
         />
+
         <Stack.Screen name="Register" component={RegisterScreen} />
+
         <Stack.Screen name="Login" component={LoginScreen} />
 
         <Stack.Screen name="Users" component={UsersScreen} />
+
         <Stack.Screen name="User" component={UserScreen} />
-        <Stack.Screen name="User Logins" component={UserLoginsScreen} />
+
+        <Stack.Screen name="User Logins" component={UsersLoginsScreen} />
+
         <Stack.Screen name="Visits" component={VisitsScreen} />
+
         <Stack.Screen
           name="Interests Places"
           component={InterestsPlacesScreen}
+        />
+
+        <Stack.Screen name="Interests Place" component={InterestsPlaceScreen} />
+
+        <Stack.Screen
+          name="Create Interests Place"
+          component={CreateInterestsPlace}
         />
       </Stack.Navigator>
     </View>
