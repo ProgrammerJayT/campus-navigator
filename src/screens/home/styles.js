39a6c0ff7d35647fbb046a59757dbca5702e317a
ProgrammerJayT <theanthem8@gmail.com>
2024-06-03 00:36:56 +0200

    Add more styles

--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -4,16 +4,16 @@ import { AppColors } from "../../constants/colors";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    position: "relative",
-    backgroundColor: "#fff",
+    backgroundColor: "red",
   },
 
   content: {
     flex: 1,
-    paddingBottom: 80,
+    paddingBottom: 50,
   },
 
   logo: {
+    marginTop: 50,
     width: "100%",
     height: 250,
   },
@@ -28,7 +28,7 @@ export const styles = StyleSheet.create({
   button: {
     flex: 1,
     alignItems: "center", // Center children horizontally
-    backgroundColor: AppColors.secondary,
+    backgroundColor: AppColors.primary,
     margin: 10,
     paddingVertical: 20,
     borderRadius: 10,
@@ -43,6 +43,37 @@ export const styles = StyleSheet.create({
 
   map: {
     ...StyleSheet.absoluteFillObject,
-    flex: 1,
+    // flex: 2,
+  },
+
+  usernameText: {
+    color: AppColors.primary,
+    textAlign: "center",
+    padding: 20,
+    fontWeight: "bold",
+  },
+
+  usernameButton: {
+    borderRadius: 10,
+    backgroundColor: AppColors.background,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+
+  userTypeText: {
+    textAlign: "center",
+    fontSize: 10,
+    color: AppColors.background,
+  },
+
+  userTypeTextContainer: {
+    padding: 0,
+    borderRadius: 10,
+    backgroundColor: AppColors.primary,
   },
 });
