88d5cd2181dd5e8c5cd3c3e4fc931fa8c6c98efb
ProgrammerJayT <theanthem8@gmail.com>
2024-06-02 22:18:25 +0200

    Remove unused styles

--- src/screens/interests-places/one/styles.js
+++ src/screens/interests-places/one/styles.js
@@ -13,36 +13,9 @@ export const styles = StyleSheet.create({
     marginTop: 20,
   },
 
-  title: {
-    color: AppColors.background,
-    flex: 1,
-    fontSize: 15,
-    textAlign: "center",
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-  },
-
   buttonsContainer: {
     flexDirection: "row",
     alignItems: "center",
     paddingHorizontal: 10,
-  },
-
-  headerContainer: {
-    paddingTop: 5,
-    backgroundColor: AppColors.primary,
-    elevation: 10,
-    marginHorizontal: 10,
-    borderRadius: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
   },
 });
