f1c9c7d55213068435c332d95fd8c9c3e7c0d2ce
ProgrammerJayT <theanthem8@gmail.com>
2024-06-03 00:31:53 +0200

    Create interests place bounds screen

---
+++ src/screens/bounds/all/layout/index.js
@@ -0,0 +1,150 @@
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { styles } from "./styles";
+import HeaderSection from "../sections/header";
+import InterestsPlacesBoundsList from "../sections/list";
+import {
+  createBound,
+  fetchBounds,
+} from "../../../../services/interests-places";
+import { failedRequest } from "../../../../services/exception";
+import { AppColors } from "../../../../constants/colors";
+import BoundsModalComponent from "../modal/add-bound";
+import LocationStateContext from "../../../../state-management/context/location";
+import ComponentsStateContext from "../../../../state-management/context/components";
+import Toast from "react-native-root-toast";
+
+const InterestsPlaceBoundsScreen = ({ navigation, route }) => {
+  const [bounds, setBounds] = useState([]);
+
+  const [modal, setModal] = useState(false);
+
+  const { interestsPlace } = route.params;
+
+  const [loading, setLoading] = useState(false);
+
+  const { location } = useContext(LocationStateContext);
+
+  const { lottieLoadingComponent, setLottieLoadingComponent } = useContext(
+    ComponentsStateContext
+  );
+
+  useEffect(() => {
+    //
+    const callFetchBounds = async () => {
+      setLottieLoadingComponent((lottieLoadingComponent) => ({
+        ...lottieLoadingComponent,
+        visible: true,
+      }));
+
+      const boundsResponse = await fetchBounds(interestsPlace?.id);
+
+      console.log("Received bounds", boundsResponse);
+
+      if (boundsResponse.response) {
+        setBounds([]);
+
+        Toast.show(
+          boundsResponse.response
+            ? failedRequest(boundsResponse).message
+            : boundsResponse.message,
+          {
+            duration: Toast.durations.LONG,
+            backgroundColor:
+              AppColors[boundsResponse.response ? "danger" : "success"],
+          }
+        );
+      } else {
+        setBounds(boundsResponse.bounds);
+      }
+
+      setLottieLoadingComponent((lottieLoadingComponent) => ({
+        ...lottieLoadingComponent,
+        visible: false,
+      }));
+
+      console.log(
+        "Bounds response",
+        boundsResponse.response
+          ? failedRequest(boundsResponse).message
+          : boundsResponse
+      );
+    };
+
+    callFetchBounds();
+  }, []);
+
+  const handleSubmit = async (values, { resetForm }) => {
+    console.log("vakues", values);
+
+    setLoading(true);
+    const boundResponse = await createBound({
+      latitude: location?.latitude,
+      longitude: location?.longitude,
+      interestsPlaceId: interestsPlace?.id,
+      surroundings: values?.surroundings,
+    });
+
+    setLoading(false);
+
+    if (boundResponse?.bound) {
+      resetForm();
+    }
+
+    console.log("Response", failedRequest(boundResponse).message);
+
+    Toast.show(
+      boundResponse.response
+        ? failedRequest(boundResponse).message
+        : "Bound marked successfully",
+      {
+        duration: Toast.durations.LONG,
+        backgroundColor:
+          AppColors[boundResponse.response ? "danger" : "success"],
+      }
+    );
+  };
+
+  const handleSetLoading = () => {
+    setLoading(false);
+  };
+
+  return (
+    <SafeAreaView style={[styles.container, { opacity: `${modal ? 0.1 : 1}` }]}>
+      <BoundsModalComponent
+        visible={modal}
+        setVisible={(visibility) => {
+          setModal(visibility);
+        }}
+        coords={location}
+        submit={handleSubmit}
+        setLoading={handleSetLoading}
+        loading={loading}
+      />
+      <View style={styles.content}>
+        <HeaderSection title={"Bounds"} />
+        <Text style={styles.title}>{interestsPlace?.name || "No title"}</Text>
+
+        <InterestsPlacesBoundsList bounds={bounds} />
+      </View>
+
+      <View style={styles.addButtonContainer}>
+        <TouchableOpacity
+          onPress={() => setModal(true)}
+          style={{ backgroundColor: "black", borderRadius: 10 }}
+        >
+          <Text style={[styles.title, { color: AppColors.background }]}>
+            New
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default InterestsPlaceBoundsScreen;
