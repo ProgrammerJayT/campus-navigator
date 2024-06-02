49bd30227fe492d65484987e771289bc85024d35
ProgrammerJayT <theanthem8@gmail.com>
2024-06-03 00:43:25 +0200

    Create users layout styles

---
+++ src/screens/users/layout/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from "react-native";
+import { AppColors } from "../../../constants/colors";
+
+export const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: AppColors.background,
+  },
+  container: {
+    flex: 1,
+    paddingBottom: 10,
+  },
+
+  loadingTextContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    marginVertical: 20,
+    fontSize: 20,
+    textAlign: "center",
+  },
+
+  buttonContainer: {
+    width: "100%",
+    paddingHorizontal: "25%",
+  },
+
+  headerContainer: {
+    elevation: 10,
+    backgroundColor: AppColors.secondary,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
